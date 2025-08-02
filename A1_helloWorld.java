14197189e29df9797e38407f952ee3b7f3d2e325
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-02 13:18:40 +0530

    First Commit

--- A1_helloWorld.java
+++ A1_helloWorld.java
@@ -1,5 +1,5 @@
 public class A1_helloWorld {
-    public static void main(String args[]){
+    public static void main(String[] args){
         
         System.out.println("Hello World");
         System.out.println("Checking Git Changes");
