24236d23cb5fbf717a99bc343500487e54c53e07
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-02 17:49:03 +0530

    Data Types

---
+++ A2_DataType.java
@@ -0,0 +1,13 @@
+public class A2_DataType {
+    public static void main(String[] args){
+        int b = 12;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean va = false;
+        System.out.println(va);
+        float price = 24.6F;
+        System.out.println(price);
+
+    }
+}
