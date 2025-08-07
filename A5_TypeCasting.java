a3aa5327920f0dc873dcd915a6c95c6a730a001a
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-07 23:41:43 +0530

    Type Casting

---
+++ A5_TypeCasting.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class A5_TypeCasting {
+
+        public static void main(String[] args) {
+
+            float marks = 12.234f;
+            int marks2 = (int) marks;
+            System.out.println(marks2);
+
+            char ch = 'a';
+            int number = ch;
+            System.out.println(number); // asci values
+
+        }
+
+}
