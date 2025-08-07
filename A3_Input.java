d43ab61785a612d7bb8904952d253726d320f372
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-07 23:18:12 +0530

    Input Code

---
+++ A3_Input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class A3_Input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String input = sc.nextLine(); // To take input with space
+        System.out.println(input);
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();  // To take numeric values
+        System.out.println(age);
+
+
+        System.out.print("Enter your Marks: ");
+        Float marks = sc.nextFloat(); // To take decimal
+        System.out.println(marks);
+    }
+}
