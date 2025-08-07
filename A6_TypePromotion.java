9812ace4052ad7f7e752cff3c4aa514f28e7a78c
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-08 00:00:50 +0530

    Type Promotion

---
+++ A6_TypePromotion.java
@@ -0,0 +1,15 @@
+public class A6_TypePromotion {
+    public static void main(String[] args){
+        char a = 'a';
+        char b = 'b';
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(b-a); // Type Promotion automatically
+
+        short x = 5;
+        byte y =  25;
+        char z = 'c';
+        byte bt = (byte) ( x + y + z); // type cast
+        System.out.println(bt);
+    }
+}
