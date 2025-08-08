29e838a4fc7e913d71c0f4769e1b67d71327b91e
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-08 10:26:11 +0530

    If Else Condition

---
+++ A8_ifElse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class A8_ifElse {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your age: ");
+        int age = sc.nextInt();
+
+        if(age>=18){
+            System.out.println("Adult");
+        } else{
+            System.out.println("Not Adult");
+        }
+    }
+}
