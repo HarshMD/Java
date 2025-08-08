9150283ad10e9e3e8934d9f1477af55b57ed06f8
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-09 00:15:24 +0530

    Sum of n natural numbers

---
+++ A17_SumFirstnNaturalNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A17_SumFirstnNaturalNumbers {
+    public static void main(String[] args){
+        int n = 6;
+        int sum = 0;
+
+        for(int i=0; i<=n; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+    }
+}
