25c19b2e9ef2362a0678406e816a09ae4059461f
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-08 11:28:11 +0530

    Switch Case

---
+++ A14_SwitchCase.java
@@ -0,0 +1,21 @@
+public class A14_SwitchCase {
+    public static void main(String[] args){
+
+        int num = 2;
+        switch(num){
+            case 1:
+                System.out.print("Samosa");
+                break;
+            case 2:
+                System.out.print("Mango");
+                break;
+            case 3:
+                System.out.print("Dosa");
+                break;
+            default:
+                System.out.print("Nothing");
+                break;
+
+        }
+    }
+}
