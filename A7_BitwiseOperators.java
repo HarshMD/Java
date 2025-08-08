deb7b79fd531fb356eccccfdb7177e916178282d
Harsh Dhanawade <harshd2911@gmail.com>
2025-08-08 10:04:39 +0530

    Bitwise Operators

---
+++ A7_BitwiseOperators.java
@@ -0,0 +1,14 @@
+public class A7_BitwiseOperators {
+    public static void main(String[] args){
+        int p = 9, q = 10;
+        System.out.println( p|q ); // Bitwise OR
+        System.out.println( p&q ); // Bitwise AND
+        System.out.println( p^q ); // Bitwise XOR
+
+        System.out.println( p<<1 ); // Left Shift 1 Time
+        System.out.println( p<<2 ); // Left Shift 2 Time
+
+        System.out.println( q>>1 ); // Right Shift 1 Time
+        System.out.println( q>>2 ); // Right Shift 2 Time
+    }
+}
