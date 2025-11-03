df92710a1372475114afb68557b335037cd03159
Harsh Dhanawade <harshd2911@gmail.com>
2025-11-04 00:18:23 +0530

    Print Prime numbers in given range

---
+++ A29_PrintPrime.java
@@ -0,0 +1,27 @@
+public class A29_PrintPrime {
+
+    public static boolean isprime(int n){
+        if(n == 2){
+            return true;
+        }
+
+        for(int i = 2; i<=Math.sqrt(n); i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void PrimeInRange(int n){
+        for(int i=2; i<=n; i++) {
+            if (isprime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        PrimeInRange(20);
+    }
+}
