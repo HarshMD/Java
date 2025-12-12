4bdcb1b94bf0362691f5e4143bc61271914f5985
Harsh Dhanawade <harshd2911@gmail.com>
2025-12-12 13:52:37 +0530

    Inverted Half Pyramid Pattern

--- A33_InvertedHalfPyramid.java
+++ A33_InvertedHalfPyramid.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class A35_InvertedHalfPyramid {
+public class A33_InvertedHalfPyramid {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
