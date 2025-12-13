6526b941265f13850476095b327ab390e16e6840
Harsh Dhanawade <harshd2911@gmail.com>
2025-12-13 10:32:50 +0530

    Butterfly Pattern Problem

---
+++ A36_ButterflyPattern.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class A36_ButterflyPattern {
+
+    public static void butterfly(int n){
+        //1st half
+        for(int i=1; i<=n; i++){
+            //star - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            //spaces - 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            //stars - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //2nd half
+        for(int i=n; i>=1; i--){
+            //star - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            //spaces - 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            //stars - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+        butterfly(n);
+    }
+}
