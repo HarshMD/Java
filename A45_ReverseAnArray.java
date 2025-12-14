791b8660596284cda335962712a16306481c1b3c
Harsh Dhanawade <harshd2911@gmail.com>
2025-12-14 12:11:26 +0530

    Reverse an Array

---
+++ A45_ReverseAnArray.java
@@ -0,0 +1,24 @@
+public class A45_ReverseAnArray {
+    public static void reverse(int[] numbers){
+        int first=0, last=numbers.length-1;
+
+        while(first<last){
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, 9, 10, 14, 17};
+
+        reverse(numbers);
+        for(int i=0; i<numbers.length; i++){
+            System.out.print(numbers[i] + " ");
+        }
+
+    }
+}
