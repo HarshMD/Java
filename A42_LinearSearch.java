462b75dd80768001331ce35ece3428a38d223924
Harsh Dhanawade <harshd2911@gmail.com>
2025-12-14 11:10:37 +0530

    Class

---
+++ A42_LinearSearch.java
@@ -0,0 +1,23 @@
+public class A42_LinearSearch {
+    public static int linearSearch(int[] numbers, int key){
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, 9, 10, 14, 17};
+        int key = 10;
+
+        int index = linearSearch(numbers, key);
+        if(index == -1){
+            System.out.println("Key not found");
+        } else{
+            System.out.println("Key found at index: " + index);
+        }
+
+    }
+}
