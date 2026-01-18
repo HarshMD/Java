fdd239f473f133dfbfc6dadf794e5b486895a7fc
Harsh Dhanawade <harshd2911@gmail.com>
2026-01-18 11:12:58 +0530

    OOPM Basics

---
+++ A52_OOPS.java
@@ -0,0 +1,45 @@
+public class A52_OOPS {
+    public static void main(String[] args){
+        Pen p1 = new Pen(); // created a pen object called p1
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
+
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "Harsh";
+        // myAcc.password = "abcdef";
+
+    }
+}
+
+class BankAccount{
+    public String username;
+    private String password;
+    public void setPassword(String pwd){
+        password = pwd;
+    }
+}
+
+class Pen{
+    String color;
+    int tip;
+
+    String getColor(){
+        return this.color;
+    }
+
+    int getTip(){
+        return this.tip;
+    }
+    void setColor(String newColor){
+        this.color = newColor;
+    }
+
+    void setTip(int tip){
+        this.tip = tip;
+    }
+}
+
